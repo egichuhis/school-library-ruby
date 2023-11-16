092c9a296e333b865eb4ef79c5cd03f52f0f4fd4
Edwin Gichuhi <edwingichuhis@gmail.com>
2023-11-16 17:55:13 +0300

    Append new data to existing json data

--- savedata.rb
+++ savedata.rb
@@ -30,20 +30,22 @@ class StoringData
   end
 
   def save_data
-    people_data = {
-      people: @app.people.map(&:to_json)
-    }
+    # Save people data
+    existing_people_data = File.exist?('people.json') ? JSON.parse(File.read('people.json'))['people'] : []
+    new_people_data = @app.people.map(&:to_json)
+    combined_people_data = (existing_people_data + new_people_data).uniq
+    File.write('people.json', JSON.pretty_generate(people: combined_people_data))
 
-    books_data = {
-      books: @app.books.map(&:to_json)
-    }
+    # Save books data
+    existing_books_data = File.exist?('books.json') ? JSON.parse(File.read('books.json'))['books'] : []
+    new_books_data = @app.books.map(&:to_json)
+    combined_books_data = (existing_books_data + new_books_data).uniq
+    File.write('books.json', JSON.pretty_generate(books: combined_books_data))
 
-    rentals_data = {
-      rentals: @app.rentals.map(&:to_json)
-    }
-
-    File.write('people.json', JSON.pretty_generate(people_data))
-    File.write('books.json', JSON.pretty_generate(books_data))
-    File.write('rentals.json', JSON.pretty_generate(rentals_data))
+    # Save rentals data
+    existing_rentals_data = File.exist?('rentals.json') ? JSON.parse(File.read('rentals.json'))['rentals'] : []
+    new_rentals_data = @app.rentals.map(&:to_json)
+    combined_rentals_data = (existing_rentals_data + new_rentals_data).uniq
+    File.write('rentals.json', JSON.pretty_generate(rentals: combined_rentals_data))
   end
 end
